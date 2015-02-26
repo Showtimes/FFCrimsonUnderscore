2275a7696c02507322046ae464a7eed0af1341f4
James William Graham <wittedhaddock@gmail.com>
2015-02-26 13:40:40 -0500

    text field on return event: chaining sequencer added

--- Demo/FFCrimsonUnderscoreDemo/FFCrimsonUnderscoreDemo/ViewController.m
+++ Demo/FFCrimsonUnderscoreDemo/FFCrimsonUnderscoreDemo/ViewController.m
@@ -8,30 +8,51 @@
 
 #import "ViewController.h"
 #import "FFCrimsonUnderscore.h"
-@interface ViewController () <FFValidationClientDelegate>
+@interface ViewController () <FFValidationClientDelegate, UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *firstName;
 @property (weak, nonatomic) IBOutlet UITextField *lastName;
 @property (weak, nonatomic) IBOutlet UITextField *email;
-
 @property (weak, nonatomic) IBOutlet UITextField *password;
 @property (strong, nonatomic) FFValidationClient *client;
 
+//meta properties
+@property (strong, nonatomic) NSArray *fields;
 @end
 
 @implementation ViewController
 
+- (NSArray *)fields{
+    if (!_fields) {
+        _fields = [NSArray arrayWithObjects: self.firstName, self.lastName, self.email, self.password, nil];
+    }
+    return _fields;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSArray *viewsNecessitatingValidation = @[self.firstName, self.lastName, self.email, self.password];
-    _client = [[FFValidationClient alloc] initWithViews:viewsNecessitatingValidation];
+    
+    //text field delegates set to this vc
+    for (UITextField *arbitraryTF in self.fields) {
+        arbitraryTF.delegate = self;
+    }
+    
+    //create validation client
+    _client = [[FFValidationClient alloc] initWithViews:self.fields];
     _client.delegate = self;
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    NSUInteger index = [self.fields indexOfObject:textField];
+    if (index + 1 == self.fields.count) {
+        [self.view endEditing:YES];
+    }
+    else {
+        [((UITextField *)[self.fields objectAtIndex:index + 1]) becomeFirstResponder];
+    }
+    
+    return YES;
 }
 
 @end
