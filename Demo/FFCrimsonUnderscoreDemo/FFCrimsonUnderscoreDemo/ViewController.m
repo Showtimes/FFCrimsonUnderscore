06acefd94fd9ffdd85e95d33adcea2874994e082
James William Graham <wittedhaddock@gmail.com>
2015-02-26 14:09:11 -0500

    Implemented the main delegate method from validation client—asynchronous alerting visible

--- Demo/FFCrimsonUnderscoreDemo/FFCrimsonUnderscoreDemo/ViewController.m
+++ Demo/FFCrimsonUnderscoreDemo/FFCrimsonUnderscoreDemo/ViewController.m
@@ -43,6 +43,8 @@
     
 }
 
+#pragma mark - Delegate methods
+
 - (BOOL)textFieldShouldReturn:(UITextField *)textField{
     NSUInteger index = [self.fields indexOfObject:textField];
     if (index + 1 == self.fields.count) {
@@ -54,5 +56,104 @@
     
     return YES;
 }
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    
+    NSString *textAfterReplacement = [textField.text stringByReplacingCharactersInRange:range withString:string];
+    
+    textField.text = textAfterReplacement;
+    
+    [self.client startValidationWithIndex:[self.fields indexOfObject:textField] reverseOrder:YES];
+    
+    return NO;
+}
+#pragma mark - Validation Client Delegate methods
+
+- (BOOL)validationClient:(FFValidationClient *)validationClient isViewValid:(UIView *)view errorAttributedText:(NSAttributedString *__autoreleasing *)errorAttributedText{
+    if ([view isEqual:self.firstName]) {
+        
+        //conditions for first name
+        
+        if (self.firstName.text.length == 0) {
+            *errorAttributedText = [[NSAttributedString alloc] initWithString:@"First name cannot be empty"
+                                                                   attributes:@{
+                                                                                NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:8.0f],
+                                                                                NSForegroundColorAttributeName: [UIColor redColor]
+                                                                                }];
+            return NO;
+        }
+        
+        if ([self.firstName.text isEqualToString:@"James"]){
+            *errorAttributedText = [[NSAttributedString alloc] initWithString:@"Mine, too!"
+                                                                   attributes:@{
+                                                                                NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:8.0f],
+                                                                                NSForegroundColorAttributeName: [UIColor blueColor]
+                                                                                }];
+            return NO;
+        }
+    }
+    else if ([view isEqual:self.lastName]){
+        //conditions for last name
+        
+        if (self.lastName.text == 0) {
+            *errorAttributedText = [[NSAttributedString alloc] initWithString:@"Last name cannot be empty"
+                                                                   attributes:@{
+                                                                                NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:8.0f],
+                                                                                NSForegroundColorAttributeName: [UIColor redColor]
+                                                                                }];
+            return NO;
+        }
+    }
+    else if ([view isEqual:self.email] ){
+        //conditions for email
+        
+        
+        //email validity using a basic regex
+        NSString *emailRegEx = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+        NSPredicate *emailPredicate = [NSPredicate predicateWithFormat:@"self matches %@", emailRegEx];
+        
+        if(![emailPredicate evaluateWithObject:self.email.text]){
+            *errorAttributedText = [[NSAttributedString alloc] initWithString:@"Incorrect email format"
+                                                                   attributes:@{
+                                                                                NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:8.0f],
+                                                                                NSForegroundColorAttributeName: [UIColor redColor]
+                                                                                }];
+            return NO;
+        }
+        
+        if([self.email.text isEqualToString:@"wittedhaddock@gmail.com"]){
+            *errorAttributedText = [[NSAttributedString alloc] initWithString:@"No Way! Same here!"
+                                                                   attributes:@{
+                                                                                NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:8.0f],
+                                                                                NSForegroundColorAttributeName: [UIColor blueColor]
+                                                                                }];
+            return NO;
+        }
+    }
+    
+    else if ([view isEqual:self.password]){
+        if (self.password.text.length == 0) {
+            *errorAttributedText = [[NSAttributedString alloc] initWithString:@"Password cannot be empty"
+                                                                   attributes:@{
+                                                                                NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:8.0f],
+                                                                                NSForegroundColorAttributeName: [UIColor blueColor]
+                                                                                }];
+            
+            return NO;
+        }
+        else if (self.password.text.length < 6) {
+            *errorAttributedText = [[NSAttributedString alloc] initWithString:@"Password cannot be less than 6 characters"
+                                                                   attributes:@{
+                                                                                NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:8.0f],
+                                                                                NSForegroundColorAttributeName: [UIColor blueColor]
+                                                                                }];
+            return NO;
+        }
+    }
+    
+    
+    return YES;
+}
+
 
 @end
